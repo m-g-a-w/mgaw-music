1d67e001de2f7c86653e1c958aeb687d7cbe1857
f1-：） <surprise_a@163.com>
2024-09-13 18:17:06 +0800

    初始化项目

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import routes from './router'
+import { useRoutes } from 'react-router-dom'
 
 function App() {
-  return <div className="App">app</div>
+  return <div className="App">{useRoutes(routes)}</div>
 }
 
 export default App
---
+++ src/views/discover/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+const Discover = () => {
+  return <div>discover</div>
+}
+export default Discover
