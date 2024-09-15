4b0d56b8e3c0779dc4ae734e4fcb41f28f2524c5
mgaw <surprise_a@163.com>
2024-09-15 19:36:42 +0800

    配置axios

--- src/views/discover/c-views/recommend/index.tsx
+++ src/views/discover/c-views/recommend/index.tsx
@@ -1,11 +1,19 @@
-import React, { memo } from 'react'
+import React, { memo, useState, useEffect } from 'react'
 import type { FC, ReactNode } from 'react'
+import hyRequest from '@/service'
 
 interface IProps {
   children?: ReactNode
 }
 
 const Recommend: FC<IProps> = () => {
+  const [count, setCount] = useState(0)
+  //测试网络请求
+  useEffect(() => {
+    hyRequest.get({ url: '/banner' }).then((res) => {
+      console.log(res)
+    })
+  }, [])
   return <div>Recommend</div>
 }
 export default memo(Recommend)
